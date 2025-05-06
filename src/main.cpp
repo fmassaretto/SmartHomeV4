f997df6239df2b0d009099902597f32f80773913
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2025-05-05 23:46:23 -0300

    Add AP connection; cleaning the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include <HtmlToString.h>
+#include <Arduino.h>
 #ifdef ESP32
-#include <WiFi.h>
-#include <AsyncTCP.h>
+  #include <WiFi.h>
+  #include <AsyncTCP.h>
 // #elif defined(ESP8266)
 // #include <ESP8266WiFi.h>
 // #include <ESPAsyncTCP.h>
@@ -11,23 +11,29 @@
 #include <WiFiClientSecure.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <ESPmDNS.h>
 #include <credentials.h>
 
 // WiFi and MQTT
-const char *ssid = WIFI_SSID;
-const char *password = WIFI_PASSWORD;
-const char *mqtt_server = HIVEMQ_MQTT_SERVER; // using HiveMQ Cloud
-const char *mqtt_username = HIVEMQ_MQTT_USERNAME;
-const char *mqtt_password = HIVEMQ_MQTT_PASSWORD;
-const int mqtt_port = 8883;
+const char *ssid              = WIFI_SSID;
+const char *password          = WIFI_PASSWORD;
+const char *soft_ap_ssid      = SOFT_AP_SSID;
+const char *soft_ap_password  = SOFT_AP_PASSWORD; // NULL for no password
+const int   channel           = 10;    // WiFi Channel number between 1 and 13
+const bool  hide_SSID         = false; // To disable SSID broadcast -> SSID will not appear in a basic WiFi scan
+const int   max_connection    = 2;  
+const char *mqtt_server       = HIVEMQ_MQTT_SERVER; // using HiveMQ Cloud
+const char *mqtt_username     = HIVEMQ_MQTT_USERNAME;
+const char *mqtt_password     = HIVEMQ_MQTT_PASSWORD;
+const int mqtt_port           = 8883;
 
 // GPIOs
 const int NUM_CHANNELS = 4;
-const int ledPins[] = {23, 22, 21, 19};
-const int buttonPins[] = {34, 35, 32, 33};
+const int lightPins[] = {23, 22, 21, 19};
+const int buttonPins[] = {32, 33, 25, 26};
 // const String deviceName[] = {"Luz Cozinha", "Luz Lavanderia", "Luz Quintal", "Luz Quarto Fabio"}; // for now not used yet
 bool lightStates[NUM_CHANNELS] = {false, false, false, false};
-bool lastButtonStates[NUM_CHANNELS] = {HIGH, HIGH, HIGH, HIGH};
+bool lastButtonStates[NUM_CHANNELS] = {LOW, LOW, LOW, LOW};
 bool mqttOnline = false;
 
 // Topics
@@ -35,13 +41,12 @@ const char *control_topic = "home/light/control";
 const char *command_topic = "home/light/command";
 
 // Globals
-// WiFiClient espClient;
 WiFiClientSecure espClient;
 PubSubClient mqttClient(espClient);
 AsyncWebServer server(80);
 AsyncEventSource events("/events");
 
-IPAddress local_IP(192, 168, 0, 222); // Defina o IP de acordo com a sua rede
+IPAddress local_IP(192, 168, 0, 222); // Defina o IP
 IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 0, 0);
 IPAddress primaryDNS(8, 8, 8, 8);   // optional
@@ -53,14 +58,14 @@ TaskHandle_t TaskButtonsHandle, TaskMQTTHandle;
 void toggleLight(int index, bool newState)
 {
   lightStates[index] = newState;
-  digitalWrite(ledPins[index], newState ? HIGH : LOW);
+  digitalWrite(lightPins[index], newState ? HIGH : LOW);
   char topic[64], payload[8];
   sprintf(topic, "home/light%d/state", index);
-  sprintf(payload, newState ? "ON" : "OFF");
+  sprintf(payload, newState ? "OFF" : "ON");
   if (mqttOnline)
     mqttClient.publish(topic, payload);
 
-  sprintf(topic, "ch%d:%s", index, newState ? "ON" : "OFF");
+  sprintf(topic, "ch%d:%s", index, newState ? "OFF" : "ON");
   events.send(topic, "update", millis());
 }
 
@@ -71,7 +76,7 @@ void toggleLight(int index)
   {
     char topic[64], payload[8];
     sprintf(topic, "home/light%d/control", index);
-    sprintf(payload, lightStates[index] ? "ON" : "OFF");
+    sprintf(payload, lightStates[index] ? "OFF" : "ON");
     mqttClient.publish(topic, payload);
   }
 }
@@ -183,41 +188,67 @@ void TaskButtons(void *parameter)
     vTaskDelay(20 / portTICK_PERIOD_MS);
   }
 }
-
+// const String index_file = htmlFileToString("index.html"); // to be tested; TODO: in future move all website to an external server outside the esp
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE html><html><head>
+ <!DOCTYPE html><html><head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ESP32 Smart Home v2</title>
     <style>
-      body { font-family: sans-serif; text-align: center; margin-top: 30px; }
-      .channel { margin: 20px; }
-      .channels_title { margin: 20px; display: inline-block; position: relative; }
-      #channels { display: flex; flex-wrap: wrap; justify-content: center;}
-      .switch {position: relative; display:grid; width: 96px; height: 34px} 
-      .switch input {display: none}
-      .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ff0000; border-radius: 6px}
-      .slider:before {position: absolute; content: ""; height: 20px; width: 30px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
-      input:checked+.slider {background-color: #3cff00}
-      input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+      body {font-family: sans-serif; text-align: center; margin-top: 30px;}
+      .channel {display:flex; flex-direction: column; width: 150px; margin: 10px;}
+      #channels {display: flex; flex-wrap: wrap; justify-content: center;}
+      .switch {position: relative; display:inline-block; width: 60px; height: 34px; left: 30%;} 
+      #enable_channels_input {width: 100px; margin-right: 10px;}
+
+      input:disabled+.slider {background-color: #ccc;}
+      .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ff0000; transition:.4s;}
+      .slider:before {position: absolute; content: ""; height: 26px; width: 26px; left: 4px; bottom: 4px; background-color: #fff; -webkit-transition: .4s; transition: .4s;}
+      input:checked+.slider {background-color: #66bb6a}
+      input:checked+.slider:before {-webkit-transform: translateX(26px); transform: translateX(26px)}
+      .slider.round {border-radius: 34px;}
+      .slider.round:before {border-radius: 50%;}
     </style>
   </head><body>
     <h2>ESP32 Smart Home v2</h2>
-    <div class="channels_title"><h3>Light Control</h3></div>
+    <div class="enable-channels"><span>Enable all channels password: </span><input type="text" name="enable_channels_input" id="enable_channels_input"><input type="button" value="Send" onclick="enable_channels()"/></div>
+    <div class="channels_title"><h3>Controle das Luzes</h3></div>
     <div id="channels">
-      <div class="channel" id="channel0">Light 1: <span id="state0">--</span> <label class="switch"> <input type="checkbox" onchange="toggle(0)" id="switch0"><span class="slider"></span></label></div>
-      <div class="channel" id="channel1">Light 2: <span id="state1">--</span> <label class="switch"> <input type="checkbox" onchange="toggle(1)" id="switch1"><span class="slider"></span></label></div>
-      <div class="channel" id="channel2">Light 3: <span id="state2">--</span> <label class="switch"> <input type="checkbox" onchange="toggle(2)" id="switch2"><span class="slider"></span></label></div>
-      <div class="channel" id="channel3">Light 4: <span id="state3">--</span> <label class="switch"> <input type="checkbox" onchange="toggle(3)" id="switch3"><span class="slider"></span></label></div>
+      <div class="channel">Cozinha: <span id="state0">--</span> <label class="switch"> <input type="checkbox" onchange="toggle(0)" class="toggle" id="switch0"> <div class="slider round"></div></label></div>
+      <div class="channel">Lavanderia: <span id="state1">--</span> <label class="switch"> <input type="checkbox" onchange="toggle(1)" class="toggle" id="switch1"> <div class="slider round"></div></label></div>
+      <div class="channel">Corredor Quintal: <span id="state2">--</span> <label class="switch"> <input type="checkbox" onchange="toggle(2)" class="toggle" id="switch2"> <div class="slider round"></div></label></div>
+      <div class="channel">Quarto do Fabio: <span id="state3">--</span> <label class="switch"> <input type="checkbox" onchange="toggle(3)" class="toggle" id="switch3" disabled> <div class="slider round"></div></label></div>
     </div>
   <script defer>
+    window.onload = resetAllToggles();
+    var bedroomSwitch = document.getElementById("switch3");
+
   function toggle(ch) {
     fetch("/toggle?ch=" + ch);
   }
+
+  function enable_channels(){
+    var password = document.getElementById("enable_channels_input");
+
+    if (password.value === "lmi56n") {
+      
+      bedroomSwitch.removeAttribute("disabled");
+      alert("All channels are enabled");
+    }
+  }
+
+  function resetAllToggles() {
+    document.getElementsByClassName("toggle").checked = false;
+  }
+
   if (!!window.EventSource) {
     const sourceEvents = new EventSource('/events');
     sourceEvents.addEventListener("update", function(e) {
       const [ch, state] = e.data.split(":");
       var index = ch.replace("ch", "");
+
+      if(index == "3" && bedroomSwitch.hasAttribute("disabled")) {
+        return;
+      }
 
       document.getElementById("state" + index).textContent = state;
       document.getElementById("switch" + index).checked = state === 'ON' ? true : false;
@@ -248,15 +279,19 @@ void setupLights()
 {
   for (int i = 0; i < NUM_CHANNELS; i++)
   {
-    pinMode(ledPins[i], OUTPUT);
+    int lightIdx = lightPins[i];
+    pinMode(lightIdx, OUTPUT);
     pinMode(buttonPins[i], INPUT_PULLUP);
-    digitalWrite(ledPins[i], LOW);
+    digitalWrite(lightIdx, HIGH);
   }
 }
 
 void setupWifi()
 {
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.disconnect();
   // Configures static IP address
+  Serial.println("[*] Creating ESP32 WIFI connection");
   if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS))
   {
     Serial.println("STA Failed to configure");
@@ -266,8 +301,23 @@ void setupWifi()
   while (WiFi.status() != WL_CONNECTED)
     delay(500);
 
-  Serial.println("\nWiFi connected: ");
+  Serial.print("[+] WiFi connected: ");
   Serial.println(WiFi.localIP());
+
+  Serial.println("[*] Creating ESP32 AP");
+  if (!WiFi.softAP(soft_ap_ssid, soft_ap_password, channel, hide_SSID, max_connection))
+  {
+      Serial.println("failed to start softAP");
+  }
+  Serial.print("[+] AP Created with IP Gateway ");
+  Serial.println(WiFi.softAPIP());
+  Serial.print("Soft AP SSID: \"");
+  Serial.print(soft_ap_ssid);
+  Serial.print("\", IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  if (MDNS.begin("esp32")) Serial.println("MDNS started.");
+  else Serial.println("Error! MDNS not started.");
 }
 
 void setupMqtt()
@@ -282,7 +332,7 @@ void asyncWebServerRoutes()
 {
   // Async Web Server Routes
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send_P(200, "text/html", index_html);
+    request->send(200, "text/html", index_html);
   });
 
   server.on("/toggle", HTTP_GET, [](AsyncWebServerRequest *request) {
